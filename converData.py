7a3d6f34c521e8defa85cc067c815a21372ccd2a
hyxp3r <erma2001@mail.ru>
2022-11-16 00:01:59 +0700

    Конвертирование данных вынесено в отдельный файл

---
+++ converData.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+class ConvertToDF:
+
+    def __init__(self, usersMetaClear:dict) -> None:
+        self.usersMetaClear = usersMetaClear
+        self.df = pd.DataFrame()
+
+
+    def convert(self):
+
+        for k, v in self.usersMetaClear.items():
+            if self.df.empty:
+                self.df = pd.DataFrame(v)
+                self.df["Группа в ВК"]  = k
+            else:
+                df_2 = pd.DataFrame(v)
+                df_2["Группа в ВК"]  = k
+                self.df = pd.concat([self.df, df_2], axis=0)
+
+            self.df = self.df.drop_duplicates(subset=["ID"])
+
+            return self.df
